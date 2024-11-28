df44c6aaeeece99ee918bdef65d628849d1604f5
Florence-Njeri <flonsie17@gmail.com>
2024-11-28 14:20:14 +0000

    built project one

---
+++ project_v0/build/CMakeFiles/database.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "/home/group-2-c/LabThree/project_v0/compile_commands.json"
+  "CMakeFiles/database"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/database.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
